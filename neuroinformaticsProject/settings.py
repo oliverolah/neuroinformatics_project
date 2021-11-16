336b00cfe465d746482b4518576eb9252f40e0df
oliverOlah <oliverolah13@gmail.com>
2021-11-16 11:09:48 +0100

    C2-I2-adding app connection to project

--- neuroinformaticsProject/settings.py
+++ neuroinformaticsProject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # added apps
-    '', 
+    'homeroots', 
 ]
 
 MIDDLEWARE = [
--- neuroinformaticsProject/urls.py
+++ neuroinformaticsProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from homeroots.views import home_page
 
 urlpatterns = [
+    path('', home_page), # Root of application
     path('admin/', admin.site.urls),
 ]
