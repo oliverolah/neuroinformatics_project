691e0617700fb8057da40d0238926bef1e40c492
oliverOlah <oliverolah13@gmail.com>
2021-11-26 08:59:46 +0100

    C9-I3-adding/changing new features to the model tables

--- neurons/models.py
+++ neurons/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class NeuronClass(models.Model):
    neuronClassId = models.AutoField(primary_key=True)
-   className = models.CharField(max_length=10)
+   className = models.CharField(max_length=10, unique=True)
    
    class Meta:
       verbose_name="Neuron Class"
@@ -15,8 +16,8 @@ class NeuronClass(models.Model):
 
 class Neuron(models.Model):
    neuronId = models.AutoField(primary_key=True)
-   neuronName = models.CharField(max_length=10)
-   neuronClassId = models.ForeignKey("NeuronClass", to_field="neuronClassId", on_delete=models.CASCADE, db_column="neuronClassId")
+   neuronName = models.CharField(max_length=10, unique=True)
+   neuronClassId = models.ForeignKey("NeuronClass", to_field="neuronClassId", on_delete=models.SET_NULL, null=True, blank=True, db_column="neuronClassId")
    
    class Meta:
       verbose_name="Neuron"
@@ -28,7 +29,7 @@ class Neuron(models.Model):
 
 class Edge(models.Model):
    neuronEdgeId = models.AutoField(primary_key=True)
-   neuronEdgeType = models.CharField(max_length=10)
+   neuronEdgeType = models.CharField(max_length=10, unique=True)
    
    class Meta:
       verbose_name="Edge"
@@ -43,11 +44,14 @@ class Edge(models.Model):
    # neuronReceiver == n_receptor (receive=receptor)
 class Connectome(models.Model):
    neuronConnId = models.AutoField(primary_key=True)
-   neuronSender = models.ForeignKey("Neuron", to_field="neuronId", on_delete=models.SET_NULL, null=True, related_name="n_transporter", db_column="neuronSender")
-   neuronReceiver = models.ForeignKey("Neuron", to_field="neuronId", on_delete=models.SET_NULL, null=True, related_name="n_receptor", db_column="neuronReceiver")
-   neuronConnNum = models.IntegerField(default=1)
-   neuronEdgeId = models.ForeignKey("Edge", to_field="neuronEdgeId", on_delete=models.SET_NULL, null=True, db_column="neuronEdgeId")
+   neuronSender = models.ForeignKey("Neuron", to_field="neuronId", on_delete=models.CASCADE, related_name="n_transporter", db_column="neuronSender")
+   neuronReceiver = models.ForeignKey("Neuron", to_field="neuronId", on_delete=models.CASCADE, related_name="n_receptor", db_column="neuronReceiver")
+   neuronConnNum = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
+   neuronEdgeId = models.ForeignKey("Edge", to_field="neuronEdgeId", on_delete=models.SET_NULL, null=True, blank=True, db_column="neuronEdgeId")
    
    class Meta:
       verbose_name="Connectome"
       verbose_name_plural="Connectomes"
+
+   def __str__(self):
+      return f"Connectome {self.neuronConnId}"
