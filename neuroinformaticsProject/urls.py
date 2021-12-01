c3bb66c61c43aecfa5d57cda7e15071e2c6d8a91
oliverOlah <oliverolah13@gmail.com>
2021-12-01 10:46:16 +0100

    C6-I2-tailwindcss setup with config for css-based static files

--- neuroinformaticsProject/urls.py
+++ neuroinformaticsProject/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from homeroots.views import returnHomePage
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', returnHomePage), # Root of application
     path('admin/', admin.site.urls),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
