e5fa8b2a2a624e128801a17700766a83279c13ac
oliverOlah <oliverolah13@gmail.com>
2022-01-27 14:26:20 +0100

    C5-I6-another follow up of C3-I6 adding corrections

--- neuroinformaticsProject/settings.py
+++ neuroinformaticsProject/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'homeroots',
     'neurons', 
     'theme', # This is for tailwindcss
+    'submitdata',
     
     # 3rd party apps
     'tailwind',
--- routers/db_routers.py
+++ routers/db_routers.py
@@ -54,19 +54,19 @@ class NeuronsRouter:
 
 
 class SubmitDataRouter:
-   route_app_labels = {'neurons'}
+   route_app_labels = {'submitdata'}
 
    def db_for_read(self, model, **hints):
       if model._meta.app_label in self.route_app_labels:
-         return 'neurons_db'
+         return 'submitdata_db'
       return None
 
    def db_for_write(self, model, **hints):
       if model._meta.app_label in self.route_app_labels:
-         return 'neurons_db'
+         return 'submitdata_db'
       return None
 
    def allow_migrate(self, db, app_label, model_name=None, **hints):
       if app_label in self.route_app_labels:
-         return db == 'neurons_db'
+         return db == 'submitdata_db'
       return None
