5ea7e8459410400b9be76baab0ec7aa509b92e58
oliverOlah <oliverolah13@gmail.com>
2022-01-27 17:34:57 +0100

    C15-I3-changing admin panel settings for view and search of data instances

--- neurons/admin.py
+++ neurons/admin.py
@@ -3,7 +3,29 @@ from .models import NeuronClass, Neuron, Edge, Connectome
 
 
 # Import 'neuron network' database tables/models
-admin.site.register(NeuronClass)
-admin.site.register(Neuron)
-admin.site.register(Edge)
-admin.site.register(Connectome)
+@admin.register(NeuronClass)
+class NeuronClassAdmin(admin.ModelAdmin):
+   list_display = ('neuronClassId', 'className')
+   ordering = ('neuronClassId',)
+   search_fields = ('neuronClassId', 'className')
+
+
+@admin.register(Neuron)
+class NeuronAdmin(admin.ModelAdmin):
+   list_display = ('neuronId', 'neuronName', 'className')
+   ordering = ('neuronId',)
+   search_fields = ('neuronId', 'neuronName')
+
+
+@admin.register(Edge)
+class EdgeAdmin(admin.ModelAdmin):
+   list_display = ('edgeId', 'edgeTypeName')
+   ordering = ('edgeId',)
+
+
+@admin.register(Connectome)
+class ConnectomeAdmin(admin.ModelAdmin):
+   list_display = ('connectomeId', 'neuronSender', 'neuronReceiver','numOfEdges', 'edgeTypeName')
+   ordering = ('connectomeId',)
+   search_fields = ('connectomeId', 'neuronSender__exact', 'neuronReceiver__exact')
+   
