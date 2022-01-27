e9f4bd5aa45e91a0aef9402cc6a7dc082a524f12
oliverOlah <oliverolah13@gmail.com>
2022-01-27 15:13:37 +0100

    C6-I6-adjusted admin area for submitdata database

--- submitdata/admin.py
+++ submitdata/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import SubmitData
 
 
-admin.site.register(SubmitData)
+@admin.register(SubmitData)
+class SubmitDataAdmin(admin.ModelAdmin):
+   list_display = ('submitId', 'title', 'firstName', 'lastName')
+   search_fields = ('submitId', 'title', 'firstName', 'lastName')
+   list_filter = ('submitId', 'title', 'firstName', 'lastName')
--- submitdata/models.py
+++ submitdata/models.py
@@ -8,12 +8,8 @@ class SubmitData(models.Model):
    lastName = models.CharField(max_length=100, verbose_name="Last Name")
    email = models.EmailField(max_length=50, verbose_name="Email Address")
    description = models.TextField(max_length=255, verbose_name="Description")
-   submitFile = models.FileField(upload_to="submitdata/")
+   submitFile = models.FileField(upload_to="submitdata/", verbose_name="Submitted File(s)")
    
    class Meta:
       verbose_name="Submit Data"
-      verbose_name_plural="Submit Data"
-   
-   def __str__(self):
-      return f"Data Submission - {self.submitId}"
-   
+      verbose_name_plural="Submit Data"   
