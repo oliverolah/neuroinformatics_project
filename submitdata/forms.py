bc9fa83feb96760839db80e58c8e3e0c2ff459f0
oliverOlah <oliverolah13@gmail.com>
2022-02-03 08:51:37 +0100

    C4-I7-styling form with tailwind and python widgets

--- submitdata/forms.py
+++ submitdata/forms.py
@@ -8,6 +8,37 @@ class SubmitDataForm(forms.ModelForm):
    class Meta:
       model =  SubmitData
       fields = ('title', 'firstName', 'lastName', 'email', 'description', 'submitFile')
+   
+   def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.fields['title'].widget.attrs.update({
+         'class': 'w-full rounded-md', 
+         'placeholder': 'Your title',
+         "required": True
+      })
+      self.fields['firstName'].widget.attrs.update({
+         'class': 'w-full rounded-md', 
+         'placeholder': 'Your first name',
+         "required": True
+      })
+      self.fields['lastName'].widget.attrs.update({
+         'class': 'w-full rounded-md', 
+         'placeholder': 'Your last name',
+         "required": True
+      })
+      self.fields['email'].widget.attrs.update({
+         'class': 'w-full rounded-md', 
+         'placeholder': 'your@email.com',
+         "required": True
+      })
+      self.fields['description'].widget.attrs.update({
+         'class': 'w-full rounded-md resize-none', 
+         'rows': '10',
+         'cols':'50', 
+         'placeholder': 'Write description here...',
+         "required": True
+      })
+
 
    def clean(self):
       cleaned_data = self.cleaned_data
