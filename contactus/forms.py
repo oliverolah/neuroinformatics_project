6beab920d8a3569c61cce158189ba91aa54faf35
oliverOlah <oliverolah13@gmail.com>
2022-02-25 10:46:53 +0100

    C4-I10-first form widget python setup

--- contactus/forms.py
+++ contactus/forms.py
@@ -5,6 +5,43 @@ from .models import ContactUsData
 
 class ContactUsDataForm(forms.ModelForm):
   
+  title = forms.CharField(widget=forms.TextInput(
+    attrs={
+      'class': 'w-full rounded-md',
+      'placeholder': 'Your title',
+      'required': True
+    }
+  ))
+  firstName = forms.CharField(widget=forms.TextInput(
+    attrs={
+      'class': 'w-full rounded-md',
+      'placeholder': 'Your first name',
+      'required': True
+    }
+  ))
+  lastName = forms.CharField(widget=forms.TextInput(
+    attrs={
+      'class': 'w-full rounded-md',
+      'placeholder': 'Your last name',
+      'required': True
+    }
+  ))
+  email = forms.EmailField(widget=forms.EmailInput(
+    attrs={
+      'class': 'w-full rounded-md',
+      'placeholder': 'your@email.com',
+      'required': True
+    }
+  ))
+  description = forms.CharField(widget=forms.Textarea(
+    attrs={
+      'class': 'w-full rounded-md',
+      'rows': '10',
+      'cols':'50',
+      'placeholder': 'Write description here...',
+      'required': True
+    }
+  ))
   class Meta:
     model = ContactUsData
     fields = (
