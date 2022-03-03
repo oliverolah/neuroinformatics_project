7b1a2df3f3d12ea0625db9f300776beddfe3fa1f
oliverOlah <oliverolah13@gmail.com>
2022-03-03 09:56:15 +0100

    C6-I11-pre preparation of the code for the email sending when submitted form

--- contactus/views.py
+++ contactus/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse # HttpResponse
 from .models import ContactUsData
 from .forms import ContactUsDataForm
+# from django.core.mail import send_mail
+# from django.conf import settings
 
 
 def returnContactUsPage(request):
@@ -10,6 +12,23 @@ def returnContactUsPage(request):
   
   if request.is_ajax():
     if form.is_valid():
+      title = form.cleaned_data['title']
+      firstName = form.cleaned_data['firstName']
+      lastName = form.cleaned_data['lastName']
+      email = form.cleaned_data['email']
+      description = form.cleaned_data['description']
+      
+      # subject = f'Contact us form new message from: {email}'
+      # body = {
+      #   'title': form.cleaned_data['title'],
+      #   'firstName': form.cleaned_data['firstName'],
+      #   'lastName': form.cleaned_data['lastName'],
+      #   'email': form.cleaned_data['email'],
+      #   'description': form.cleaned_data['description']
+      # }
+      # message = "\n".join(body.values())
+      # email_receiver = [settings.EMAIL_POST_USER]
+      # send_mail(subject, message, email, email_receiver, fail_silently = False)
       data["status"] = {
         "result": "success",
         "message": "Form valid and submitted",
