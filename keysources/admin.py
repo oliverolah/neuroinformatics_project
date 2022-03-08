4415338ecf471ba4f7510bcd267c721498f37773
oliverOlah <oliverolah13@gmail.com>
2022-03-08 18:22:14 +0100

    C1-I13-full database setu pwith postgresql

--- keysources/admin.py
+++ keysources/admin.py
@@ -1,2 +1,11 @@
 from django.contrib import admin
+from .models import KeyPapersData, KeyWebsitesData
 
+@admin.register(KeyPapersData)
+class KeyPapersDataAdmin(admin.ModelAdmin):
+  list_display = ('paperTitle',)
+  
+
+@admin.register(KeyWebsitesData)
+class KeyWebsitesDataAdmin(admin.ModelAdmin):
+  list_display = ('websiteName',)
--- keysources/models.py
+++ keysources/models.py
@@ -1,2 +1,20 @@
 from django.db import models
 
+class KeyPapersData(models.Model):
+  keySourceId = models.AutoField(primary_key=True)
+  paperTitle = models.CharField(max_length=255, verbose_name="Paper Title")
+  paperFile = models.FileField(upload_to="keypapers/", verbose_name="Paper File")
+  
+  class Meta:
+    verbose_name="Key Papers"
+    verbose_name_plural="Key Papers"
+
+
+class KeyWebsitesData(models.Model):
+  keySourceId = models.AutoField(primary_key=True)
+  websiteName = models.CharField(max_length=255, verbose_name="Website Name")
+  websiteUrl = models.URLField(max_length=255, verbose_name="Website URL")
+  
+  class Meta:
+    verbose_name="Key Websites"
+    verbose_name_plural="Key Websites"
