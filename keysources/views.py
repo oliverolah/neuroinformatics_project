1086a94436156e189cefb1220e8c59ab8994fe87
oliverOlah <oliverolah13@gmail.com>
2022-03-09 13:52:53 +0100

    C2-I14-first layout and functionality setup for both containers

--- keysources/views.py
+++ keysources/views.py
@@ -1,6 +1,23 @@
+import os
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import KeyPapersData, KeyWebsitesData
 
 
 def returnKeySourcesPage(request):
-  return render(request, "keysources/key_sources_page.html")
+  papers = KeyPapersData.objects.all()
+  websites = KeyWebsitesData.objects.all()
+  context = {
+    'papers': papers,
+    'websites': websites
+  }
+  return render(request, "keysources/key_sources_page.html", context)
+
+def download_paper(request, path):
+  file_path = os.path.join(settings.MEDIA_ROOT, path)
+  if os.path.exists(file_path):
+    with open(file_path, 'rb') as fh:
+      response = HttpResponse(fh.read(), content_type="application/paperFile")
+      response['Content-Disposition'] = 'inline;filename=' + os.path.basename(file_path)
+      return response
+  raise Http404
