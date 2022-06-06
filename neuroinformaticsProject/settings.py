7dac279230abd1fbec8c22e66b99cc8060b8fde0
oliverOlah <oliverolah13@gmail.com>
2022-06-06 10:47:38 +0200

    C11-changing dotenv settings

--- neuroinformaticsProject/settings.py
+++ neuroinformaticsProject/settings.py
@@ -2,11 +2,12 @@ from pathlib import Path
 import os
 import dotenv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Find .env file to load hidden variables
-dotenv_file = os.path.join(BASE_DIR, '.env')
+dotenv_file = os.path.join(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
