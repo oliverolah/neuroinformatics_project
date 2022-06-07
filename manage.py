89c79399709daa9e33310a373528b8617049ff05
oliverOlah <oliverolah13@gmail.com>
2022-06-07 09:11:41 +0200

    C13-removing django-dotenv from manage.py file

--- manage.py
+++ manage.py
@@ -2,13 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import dotenv
+# import dotenv
 
 
 def main():
     """Run administrative tasks."""
     # from dotenv.read_dotenv() ---> ...load_dotenv()
-    dotenv.load_dotenv()
+    # dotenv.load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neuroinformaticsProject.settings')
     try:
         from django.core.management import execute_from_command_line
